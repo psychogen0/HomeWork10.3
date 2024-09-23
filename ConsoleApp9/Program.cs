0e288aa5c56e291e5a6d52294dab43fe1489265f
Natalia <natabilcuk@gmail.com>
2024-09-23 19:18:17 +0300

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,13 @@
+﻿// если нужно использовать уже заготовленные слова
+
+//var str = " flower, trees, sky ";
+//var strWithoutSpaces = str.Replace(" ", "");
+//Console.WriteLine(strWithoutSpaces);
+
+// или если пользователь должен ввести слова сам
+
+var str = (Console.ReadLine());
+var strWithoutSpaces = str.Replace(" ", "");
+Console.WriteLine(strWithoutSpaces);
+
+// просто не совсем понимаю условие :(
